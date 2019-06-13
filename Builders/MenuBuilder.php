16299443c3eac365351428c6c9dc5222674fa888
codeperl <it.codeperl@gmail.com>
2019-06-13 14:52:55 +0600

    Fixing menu with route.

--- Builders/MenuBuilder.php
+++ Builders/MenuBuilder.php
@@ -19,12 +19,12 @@ class MenuBuilder
             return VerticalMenu::new()->addClass('list-unstyled components')
                 ->action('HomeController@index', 'Home')
                 ->submenu('<a href="#authorizationSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle collapsed">Authorization</a>', VerticalMenu::new()->setAttribute('id', 'authorizationSubmenu')->addClass('list-unstyled collapse')
-                    ->action('\Modules\Account\Http\Controllers\ResourcesController@index', 'Resources')
-                    ->action('\Modules\Account\Http\Controllers\PermissionsController@index', 'Permissions')
-                    ->action('\Modules\Account\Http\Controllers\AssignResourcesToPermissionsController@index', 'Assign resource to permission')
-                    ->action('\Modules\Account\Http\Controllers\RolesController@index', 'Roles')
-                    ->action('\Modules\Account\Http\Controllers\AssignRolesToUsersController@index', 'Assign role to user')
-                    ->action('\Modules\Account\Http\Controllers\AssignPermissionsToUsersController@index', 'Assign permission to user')
+                    ->route('resources.index', 'Resources')
+                    ->route('permissions.index', 'Permissions')
+                    ->route('assignResourcesToPermissions.index', 'Assign resource to permission')
+                    ->route('roles.index', 'Roles')
+                    ->route('assignRolesToUsers.index', 'Assign role to user')
+                    ->route('assignPermissionsToUsers.index', 'Assign permission to user')
                 )
                 ->setActiveFromRequest();
         });
